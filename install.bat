bcaaebe41278573cb8f0660061cb3505385242ca
Frank Zheng <smldragon@hotmail.com>
2024-09-29 22:31:43 -0400

    add openfx to maven depenciy

--- install.bat
+++ install.bat
@@ -1,5 +1,6 @@
 rem run it in sialibs folder where jars reside ( copy jars from spankodds_jar if they are missing)
-rem to add/delete a  dependency, need to change version of -DgroupId=sialibs.com.sia  -DartifactId=common-client-dependency ! and make sure newly added version folder is added to git to be pushed to github
+rem to add/delete a  dependency, need to change version of -DgroupId=sialibs.com.sia  -DartifactId=common-client-dependency !
+rem and make sure ALL changes( including Unversioned Files by Intellij git) are added to git to be pushed to github
 rem after running install.bat, add anything new in directory sialibs to git, commit them and push it to github
 call mvn install:install-file -Dfile=dependencies_xml -DgroupId=sialibs.com.sia  -DartifactId=common-client-dependency -Dversion=2.1.0 -Dpackaging=pom -DlocalRepositoryPath=.  -DcreateChecksum=true
 call mvn install:install-file -Dfile=swingx-action-1.6.5-1.jar -DgroupId=sialibs.swingx  -DartifactId=swingx-action -Dversion=1.6.5 -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
