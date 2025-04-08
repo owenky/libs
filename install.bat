4ae04a263f0ca1e530c2efe4aed887c5cf20c269
Frank Zheng <smldragon@hotmail.com>
2025-04-08 17:27:16 -0400

    changed kay pair

--- install.bat
+++ install.bat
@@ -4,7 +4,7 @@ rem and make sure ALL changes( including Unversioned Files by Intellij git) are 
 rem after running install.bat, add anything new in directory sialibs to git, commit them and push it to github
 rem  Last Step: add newly added jar to file dependencies_xml in this directory and change its version defined in version tag
 
-call mvn install:install-file -Dfile=dependencies_xml -DgroupId=sialibs.com.sia  -DartifactId=common-client-dependency -Dversion=3.1.2 -Dpackaging=pom -DlocalRepositoryPath=.  -DcreateChecksum=true
+call mvn install:install-file -Dfile=dependencies_xml -DgroupId=sialibs.com.sia  -DartifactId=common-client-dependency -Dversion=3.1.3 -Dpackaging=pom -DlocalRepositoryPath=.  -DcreateChecksum=true
 call mvn install:install-file -Dfile=swingx-action-1.6.5-1.jar -DgroupId=sialibs.swingx  -DartifactId=swingx-action -Dversion=1.6.5 -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
 call mvn install:install-file -Dfile=swingx-autocomplete-1.6.5-1.jar -DgroupId=sialibs.swingx  -DartifactId=swingx-autocomplete -Dversion=1.6.5 -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
 call mvn install:install-file -Dfile=swingx-common-1.6.5-1.jar -DgroupId=sialibs.swingx  -DartifactId=swingx-common -Dversion=1.6.5 -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
